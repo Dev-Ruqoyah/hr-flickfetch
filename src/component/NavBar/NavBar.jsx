378143aa94c141a5581ba133e8277b2d53e2a84d
Dev-Ruqoyah <hamzatrukayatabiodun02@gmail.com>
2025-01-08 14:59:58 +0100

    modify the navbar

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -18,37 +18,65 @@ const NavBar = () => {
           <div className="container ">
             <div className="flex justify-between items-center">
               <div className="brand-logo">
-                <h3 className="text-xl font-bold text-white">hr-flickfetch</h3>
+                <Link   to = {"/ "}>
+               
+                  <h3 className="text-xl font-bold text-white">
+                    hr-flickfetch
+                  </h3>
+                </Link>
               </div>
 
               <div className=" md:flex hidden items-center gap-3">
                 {Links.map(({ name, url }, i) => (
                   <div className="" key={i}>
-                    <NavLink to={url} className={({isActive})=>isActive?"text-red-500 font-bold transition-all duration-300":"text-white hover:text-red-500 transition-all duration-300 "}  >{name}</NavLink>
+                    <NavLink
+                      to={url}
+                      className={({ isActive }) =>
+                        isActive
+                          ? "text-red-500 font-bold transition-all duration-300"
+                          : "text-white hover:text-red-500 transition-all duration-300 "
+                      }
+                    >
+                      {name}
+                    </NavLink>
                   </div>
                 ))}
               </div>
 
               <div className="md:hidden block ">
                 {isOpen ? (
-                  <FaX className="text-white  transition-transform duration-300 transform hover:rotate-90" onClick={() => setIsOpen(false)} />
+                  <FaX
+                    className="text-white  transition-transform duration-300 transform hover:rotate-90"
+                    onClick={() => setIsOpen(false)}
+                  />
                 ) : (
-                  <FaBars className="text-white transition-transform duration-300 transform hover:scale-110"  onClick={() => setIsOpen(true)} />
+                  <FaBars
+                    className="text-white transition-transform duration-300 transform hover:scale-110"
+                    onClick={() => setIsOpen(true)}
+                  />
                 )}
               </div>
-             
             </div>
             <div
-                className={`${
-                  isOpen ? "flex animate-slide-in" : "hidden animate-slide-in"
-                } md:hidden flex-col mt-3  gap-3`}
-              >
-                {Links.map(({ name, url }, i) => (
-                  <div className="" key={i}>
-                    <NavLink to={url} className={({isActive})=>isActive?"text-red-500 font-bold transition-all duration-300":"text-white hover:text-red-500 transition-all duration-300"}>{name}</NavLink>
-                  </div>
-                ))}
-              </div>
+              className={`${
+                isOpen ? "flex animate-slide-in" : "hidden animate-slide-in"
+              } md:hidden flex-col mt-3  gap-3`}
+            >
+              {Links.map(({ name, url }, i) => (
+                <div className="" key={i}>
+                  <NavLink
+                    to={url}
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-red-500 font-bold transition-all duration-300"
+                        : "text-white hover:text-red-500 transition-all duration-300"
+                    }
+                  >
+                    {name}
+                  </NavLink>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
